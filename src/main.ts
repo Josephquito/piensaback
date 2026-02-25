fb0faae09de1bcfcc6808eb7913d79da8d845dc4
Josephquito <jdquito.3@sudamericano.edu.ec>
2026-02-25 15:37:17 -0500

    port fix

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,9 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(process.env.PORT ?? 3000);
+  const port = Number(process.env.PORT) || 3000;
+  await app.listen(port, '0.0.0.0');
+  console.log(`Listening on ${await app.getUrl()}`);
 }
 
 bootstrap();
